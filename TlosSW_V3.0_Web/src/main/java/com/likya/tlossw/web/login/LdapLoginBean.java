cf4e04b9f198153aa8770ecbce33d2e8f2b9ecf8
hakan <hakan.saribiyik@likyateknoloji.com>
2013-04-07 20:08:35 +0000

    LDAP icin; hakan

---
+++ TlosSW_V3.0_Web/src/main/java/com/likya/tlossw/web/login/LdapLoginBean.java
@@ -0,0 +1,198 @@
+package com.likya.tlossw.web.login;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.management.remote.JMXConnector;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+import com.likya.tlos.model.xmlbeans.common.RoleDocument.Role;
+import com.likya.tlos.model.xmlbeans.user.PersonDocument.Person;
+import com.likya.tlossw.model.WebSpaceWideRegistery;
+import com.likya.tlossw.model.auth.AppUser;
+import com.likya.tlossw.web.TlosSWBaseBean;
+import com.likya.tlossw.web.appmng.SessionMediator;
+import com.likya.tlossw.web.utils.LdapClient;
+
+@ManagedBean(name = LdapLoginBean.BEAN_NAME)
+@ViewScoped
+public class LdapLoginBean extends TlosSWBaseBean implements Serializable {
+
+	private static final long serialVersionUID = 2692803198850093936L;
+
+	private DirContext dirContext = null;
+
+	public static final String BEAN_NAME = "ldapLoginBean";
+
+	private static final Logger logger = Logger.getLogger(LdapLoginBean.class);
+
+	public static final String LOGIN_SUCCESS = "loginSuccess";
+	public static final String LOGIN_FAILURE = "loginFailure";
+	public static final String LOGIN_ENGINE_DIRECTOR = "loginEngineDirector";
+
+	protected Person loggedUser;
+
+	private String userName;
+	private String userPassword;
+
+	@ManagedProperty(value = "#{sessionMediator}")
+	public SessionMediator sessionMediator;
+
+	public SessionMediator getSessionMediator() {
+		return sessionMediator;
+	}
+
+	public void setSessionMediator(SessionMediator sessionMediator) {
+		this.sessionMediator = sessionMediator;
+	}
+
+	@ManagedProperty(value = "#{jmxConnectionHolder.jmxConnector}")
+	public JMXConnector jmxConnector;
+
+	public String login() {
+
+		logger.info("start : MyLdapLoginBean : login");
+
+		String returnValue;
+
+		String validated = verifyUserBean();
+
+		WebSpaceWideRegistery webSpaceWideRegistery = sessionMediator.getWebSpaceWideRegistery();
+
+		if (LOGIN_FAILURE.equals(validated)) {
+			returnValue = LOGIN_FAILURE;
+		} else if (LOGIN_SUCCESS.equals(validated)) {
+			addMessage("loginForm", "loadingMessage", "tlos.login.status", null);
+			returnValue = LOGIN_SUCCESS;
+		} else {
+			addMessage("loginForm", "errorMessage", "invalid Mode", null);
+			returnValue = LOGIN_FAILURE;
+		}
+
+		logger.info("end : Registered LdapLoginBean : login");
+
+		return returnValue;
+
+	}
+
+	public String verifyUserBean() {
+
+		// ManagerMediator mm = (ManagerMediator)
+		// FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("managerMediator");
+		// ServletContext webAppContext = (ServletContext)
+		// FacesContext.getCurrentInstance().getExternalContext().getContext();
+		// JmxUser jmxUserApp = (JmxUser) webAppContext.getAttribute("JmxUser");
+
+		String host = "Localhost";
+		String port = "10389";
+		String rootdn = "cn=carol,ou=Users,dc=likya,dc=com";
+		String username = "carol";
+		String password = "hakan123";
+
+		String INITCTX = "com.sun.jndi.ldap.LdapCtxFactory";
+		String MY_HOST = "ldap://Localhost:10389";
+		String MGR_DN = "cn=carol,ou=Users,dc=likya,dc=com";
+		String MGR_PW = "hakan123";
+
+		String identifier = "carol";
+
+		String MY_SEARCHBASE = "";
+		String MY_FILTER = "(&(cn=" + identifier + "))";
+		String MY_ATTRS[] = { "cn", "mail" };
+
+		LdapClient ldapClient = new LdapClient();
+		try {
+			ldapClient.connect(host, port, rootdn, MGR_DN, password);
+
+			if (ldapClient.getDirContext() != null) {
+				if (ldapClient.searchTest(MY_SEARCHBASE, MY_FILTER)) {
+					System.out.println("BASARILI");
+					ldapClient.disconnect();
+					return LOGIN_SUCCESS;
+				} else {
+					System.out.println("BASARISIZ !");
+				}
+			}
+		} catch (NamingException e) {
+			System.out.println("LDAP authentication could NOT Initialized. " + e.getMessage());
+			System.err.println(e);
+		}
+
+		logger.info("setting login error message ");
+
+		addMessage(null, FacesMessage.SEVERITY_ERROR, "Kullan�c� adi ya da sifresi hatali !", "Kullanici adi ya da sifresi hatali !");
+
+		return LOGIN_FAILURE;
+
+	}
+
+	private void setSessionLoginParam(boolean isLoggedIn) {
+		HttpSession httpSession = null;
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		if (facesContext != null) {
+			httpSession = (HttpSession) facesContext.getExternalContext().getSession(false);
+		}
+
+		if (httpSession != null) {
+			httpSession.setAttribute("LoggedIn", Boolean.toString(isLoggedIn));
+		}
+	}
+
+	public static void copyAppUserToPerson(AppUser appUser, Person person) {
+		person.setId(appUser.getId());
+		person.setName(appUser.getName());
+		person.setSurname(appUser.getSurname());
+		person.setRole(Role.Enum.forString(appUser.getRole().getRoleId()));
+		person.setUserPassword(appUser.getPassword());
+		person.setUserName(appUser.getUsername());
+		person.setTransformToLocalTime(appUser.isTransformToLocalTime());
+	}
+
+	/**
+	 * Disconnect from the server.
+	 */
+	public void disconnect() {
+		try {
+			if (dirContext != null) {
+				dirContext.close();
+				dirContext = null;
+			}
+		} catch (NamingException e) {
+			logger.error("Ldap client - ", e);
+		}
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public JMXConnector getJmxConnector() {
+		return jmxConnector;
+	}
+
+	public void setJmxConnector(JMXConnector jmxConnector) {
+		this.jmxConnector = jmxConnector;
+	}
+
+}
